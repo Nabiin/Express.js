151278f30ddde2152e2c243dd1203eb2461f1b2b
Nabin Bhandari <nabin123456j@gmail.com>
2020-04-18 23:28:21 +0545

    code include handlebars .hbs and 404 routs

--- src/app.js
+++ src/app.js
@@ -1,16 +1,60 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
 
-/* console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
-*/
+const app = express();
 
-const publicpath = path.join(__dirname, '../public')
+const staticPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+hbs.registerPartials(partialsPath);
 
-app.use(express.static(publicpath))
+app.use(express.static(staticPath));
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
 
- app.listen('3000' , ()=>{
-    console.log('the server is running in 3000 port')
+
+app.get("", (req, res) => {
+  res.render("index", {
+    name: "Home",
+    author: "Nabin Bhandari",
+  });
+});
+
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    name: "About us",
+    author: "Nabin Bhandari",
+  });
+});
+
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    name: "Help",
+    author: "Nabin Bhandari",
+  });
+}); 
+
+
+app.get('/help/*' , (req,res)=>{
+  res.render('404',{
+    error : 'Help article not Found',
+  })
 })
 
+app.get('/about/*' , (req,res)=>{
+  res.render('404',{
+    error : 'About Section not Found',
+  })
+})
+app.get('*' , (req, res)=>{
+  res.render('404',{
+    error : 'Page not Found',
+  })
+})
+
+app.listen("3000", () => {
+  console.log("App is listen in 3000 port");
+});
